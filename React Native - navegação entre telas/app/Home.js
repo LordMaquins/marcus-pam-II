89109665ca43a184657f36389196d97eed0f0266
Marcus <mv20040908@gmail.com>
2023-09-25 22:28:22 -0300

    tudo top

---
+++ React Native - navegação entre telas/app/Home.js
@@ -0,0 +1,51 @@
+import React, { useRef, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import * as Animatable from 'react-native-animatable';
+
+function Page() {
+  const titleRef = useRef(null);
+
+  useEffect(() => {
+    // Inicie a animação após o componente ser montado
+    titleRef.current?.bounceInUp(1000); // 1000 é a duração da animação em milissegundos
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <Animatable.View
+          ref={titleRef}
+          style={styles.titleContainer}
+        >
+          <Text style={styles.title}>Bem-vindo</Text>
+        </Animatable.View>
+        <Text style={styles.subtitle}>a Summoners Rift</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F0F0F0',
+  },
+  main: {
+    alignItems: 'center',
+  },
+  titleContainer: {
+    marginBottom: 20, // Espaço entre o título e o subtítulo
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 36,
+    color: '#38434D',
+  },
+});
+
+export default Page;
